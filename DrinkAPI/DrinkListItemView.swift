ded5cd9f01a3663b0babe7cafe5fffba00ef523e
Robert Palmer <Awriter247@gmail.com>
2024-02-05 17:37:17 -0500

    Replace list with DrinkListItem view

---
+++ DrinkAPI/DrinkListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkListItemView.swift
+//  DrinkAPI
+//
+//  Created by Robert Palmer on 2/5/24.
+//
+
+import SwiftUI
+
+struct DrinkListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkListItemView()
+}
